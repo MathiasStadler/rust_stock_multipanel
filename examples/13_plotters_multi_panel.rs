fc84e1614f8ab100aea6ff4825208fd3bffea411
Mathias <email@example.com>
2024-08-21 12:48:19 +0000

    get data from csv file

---
+++ examples/13_plotters_multi_panel.rs
@@ -0,0 +1,56 @@
+use ta::DataItem;
+
+fn main() {
+    println!("Hello, panel!");
+
+    //get data from csv
+    let stock_data: Vec<(String, f64, f64, f64, f64, f64)> = get_data_from_csv();
+
+    // show data
+    for data in stock_data.iter() {
+        match data.0 {
+            // TODO ^ Try deleting the & and matching just "Ferris"
+            _ => println!(
+                "Stock Data {} {} {} {} {} {}",
+                data.0, data.1, data.2, data.3, data.4, data.5
+            ),
+        }
+    }
+
+    // println!("{:?}",stock_data);
+}
+
+fn get_data_from_csv() -> Vec<(String, f64, f64, f64, f64, f64)> {
+    let mut reader = csv::Reader::from_path("stock_data/stock_trex_data.csv").unwrap();
+
+    let mut return_vec: Vec<(String, f64, f64, f64, f64, f64)> = Vec::new();
+
+    for record in reader.deserialize() {
+        let (date, open, high, low, close, volume): (String, f64, f64, f64, f64, f64) =
+            record.unwrap();
+
+        let dt = DataItem::builder()
+            .open(open)
+            .high(high)
+            .low(low)
+            .close(close)
+            .volume(volume)
+            .build()
+            .unwrap();
+
+        // let sma_7_val = sma_7.next(&dt);
+
+        return_vec.push((date.clone(), open, high, low, close, volume));
+
+        // println!("pop => {:?}", return_vec.pop());
+
+        // println!(
+        //     " {:?}, {:?}, {:?}, {:?},{:?}, {:?}",
+        //     date, open, high, low, close, volume
+        // );
+    }
+
+    return_vec
+}
+
+// cargo run --example 13_plotters_multi_panel
