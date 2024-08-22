d488377f72fe099a898b8f5eb4eb079eb2fd1025
Mathias <email@example.com>
2024-08-22 16:07:36 +0000

    finished for today

--- examples/20_plotters_multi_panel.rs
+++ examples/20_plotters_multi_panel.rs
@@ -1,16 +1,23 @@
 // example convert inside fn String to str
 
+// use chrono::Date;
 use chrono::ParseError;
 // use chrono::naive::NaiveDateTime;
 // use chrono::offset::{Local, TimeZone};
+// use chrono::Local;
 use chrono::NaiveDateTime;
 use chrono::{Duration, NaiveTime};
 // use plotters::prelude::*;
 use plotters::backend::BitMapBackend;
+
 use plotters::drawing::IntoDrawingArea;
 use plotters::prelude::ChartBuilder;
-use plotters::prelude::WHITE;
+
+use plotters::prelude::Circle;
 use plotters::prelude::BLUE;
+use plotters::prelude::WHITE;
+
+use plotters::style::Color;
 use plotters::style::IntoFont;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -44,6 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     // FROM HERE
+    // HIER WEITER
     // workspace_rust/rust_plotter_histo_multi_line examples/plotters_5_3_x_stock_example.rs
     let y_min = parse_time(&String::from("9:30:0")).unwrap();
     let y_max = parse_time(&String::from("13:00:0")).unwrap();
@@ -58,16 +66,19 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .caption("Time", ("sans-serif", 30.0).into_font())
         .build_cartesian_2d(from_date..to_date, y_min..y_max)?;
 
-    chart
+    _chart
         .configure_mesh()
         .light_line_style(&WHITE)
         .y_label_formatter(&|y| format!("{:02}:{:02}", y.num_minutes(), y.num_seconds() % 60))
-        .x_label_formatter(&|x| x.naive_local().to_string())
+        .x_label_formatter(&|x| x.to_string())
         .draw()?;
 
-    chart.draw_series(data.iter().map(|x| {
+    _chart.draw_series(data.iter().map(|x| {
         Circle::new(
-            (parse_datetime(x.0), parse_time(x.1).unwrap()),
+            (
+                parse_date_time(x.0.as_str()),
+                parse_time(x.1),
+            ),
             5,
             BLUE.filled(),
         )
@@ -122,11 +133,23 @@ fn parse_time(time_str: &String) -> Result<Duration, ParseError> {
     return return_date;
 }
 
-fn parse_datetime(t: &str) -> Date<Local> {
-    Local
-        .datetime_from_str(&format!("{} 0:0", t), "%Y-%m-%d %H:%M")
-        .unwrap()
-        .date()
+fn parse_date_time(date_time: &str) ->  Result<Duration, ParseError>  {
+    // check it
+    // Local
+   let return_date =  match NaiveDateTime::parse_from_str(
+        &format!("1999-01-01 {}", date_time),
+        "%Y-%m-%d %H:%M:%S%.f",
+    ) {
+        Ok(date) => Ok(date
+            .time()
+            .signed_duration_since(NaiveTime::from_hms_opt(0, 0, 0).expect("REASON"))),
+        Err(e) => {
+            println!("parse_time return_data=> {}", e);
+            Err(e)
+        }
+    };
+
+    return return_date;
 }
 
 fn get_data_from_csv() -> Vec<(String, f32, f32, f32, f32, f32)> {
